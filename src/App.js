0a4c8207be89447041e1b5beacc293af601c29f9
Abdullah Ibne Masud <2018-525-313@student.cse.du.ac.bd>
2023-05-15 03:36:21 +0600

    done

--- src/App.js
+++ src/App.js
@@ -9,12 +9,8 @@ import RatingForm from "./Feedbacks/FeedbackForms/RatingForm/RatingForm";
 import ReceivedFeedbacks from "./Feedbacks/ReceivedFeedbacks/ReceivedFeedbacks";
 import GivenFeedbacks from "./Feedbacks/GivenFeedbacks/GivenFeedbacks";
 import Dashboard from "./Dashboard/Dashboard";
-<<<<<<< HEAD
-import HomePage from "./HomePage/HomePage";
 import Profile from "./Profile/Profile";
-=======
-import HomePage from "./Homepage/HomePage";
->>>>>>> b693d1b305e14803927c66f8ccb9102a31880dc8
+
 // import ReviewForm from "./Reviews/TeacherReviews/IndividualReview/ReviewForm";
 
 // import { Navbar } from "./Navbar";
@@ -26,14 +22,14 @@ function App() {
         {/* <Navbar /> */}
         <Routes>
           <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<LoginPage />} />
           {/* <Route path="/" element={<Signup />} /> */}
           {/* <Route path="/" element={<SignupChoice />} /> */}
           {/* <Route path="/" element={<HomePage />} /> */}
           {/* <Route path="/" element={<TeacherReviews />} /> */}
           {/* <Route path="/" element={<IndividualReviews />} /> */}
           {/* <Route path="/signupChoice" element={<SignupChoice />} /> */}
-          <Route path="/home" element={<HomePage />} />
+          {/* <Route path="/home" element={<HomePage />} /> */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
